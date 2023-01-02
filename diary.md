82aa614eb6dc02230cc8279d8ca4ff136ef883ea
Tricia Amheiser <tricia@taarts.net>
2023-01-02 18:19:04 -0500

    now working, better layout

---
+++ diary.md
@@ -0,0 +1,6 @@
+going back and forth on this...
+
+Having 2 players that are manual is hard for my brain, I would love to make a variation where the computer is the opponent.
+
+VVV this does that, in a slightly different way to the MO we've been using.
+https://github.com/adaapp/rock-paper-typescript
--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,101 @@
 import './style.css'
 // import { setupCounter } from './counter'
+// import rock from './src/images/rock.png'
+// import paper from './src/images/paper.png'
+// import scissors from './src/images/scissors.png'
+
+// const images = {
+//   rock,
+// paper,
+// scissors,
+// }
+// function showRoshambo() {
+//   switch ('') {
+//     case 'rock':
+//       return rock
+//     case 'paper':
+//       return paper
+//     case 'scissors':
+//       return scissors
+//     default:
+//   }
 
 const state = {
   player1: '',
   player2: '',
 }
-alert()
-let userScore: number = 0
-let computerScore: number = 0
-let counter: number = 0
 
-function startGame() {
-  userScore = 0
-  computerScore = 0
-  counter = 0
-  outputMessage("Let's play!")
-  hideStartButton()
-}
-function outputMessage(message: string): void {
-  alert(message)
-}
-
-function hideStartButton(): void {
-  let startButton: HTMLButtonElement = document.querySelector('.start-button')
-  startButton.style.display = 'none'
+function checkGame() {
+  if (state.player1 === '' || state.player2 === '') return
+  if (state.player1 === 'rock' && state.player2 === 'rock') {
+    alert('Tie')
+  } else if (state.player1 === 'rock' && state.player2 === 'paper') {
+    alert('Player 2 wins')
+  } else if (state.player1 === 'rock' && state.player2 === 'scissors') {
+    alert('Player 1 wins')
+  }
+  if (state.player1 === 'paper' && state.player2 === 'rock') {
+    alert('Player 1 wins')
+  }
+  if (state.player1 === 'paper' && state.player2 === 'paper') {
+    alert('Tie')
+  }
+  if (state.player1 === 'paper' && state.player2 === 'scissors') {
+    alert('Player 2 wins')
+  }
+  if (state.player1 === 'scissors' && state.player2 === 'rock') {
+    alert('Player 2 wins')
+  }
+  if (state.player1 === 'scissors' && state.player2 === 'paper') {
+    alert('Player 1 wins')
+  }
+  if (state.player1 === 'scissors' && state.player2 === 'scissors') {
+    alert('Tie')
+  }
 }
 
 function setupRoshambo(id: 'player1' | 'player2') {
   const rockBtn = document.querySelector(`.${id} #rock`) as HTMLElement
   console.log(rockBtn)
-  const paperBtn = document.querySelector(`.${id} button.paper`) as HTMLElement
-  const scissorsBtn = document.querySelector(
-    `.${id} button.scissors`
-  ) as HTMLElement
+  const paperBtn = document.querySelector(`.${id} #paper`) as HTMLElement
+  console.log(paperBtn)
+  const scissorsBtn = document.querySelector(`.${id} #scissors`) as HTMLElement
+  console.log(scissorsBtn)
 
-  function handleClickButton(event: MouseEvent) {
-    const thingCLickedOn = event.target
-    if (thingCLickedOn instanceof HTMLButtonElement) {
-      if (thingCLickedOn.classList.contains('active')) {
-        console.log('Clicked on')
-      }
-    }
-  }
+  const restartBtn = document.querySelector('#restart') as HTMLElement
 
   function handleSelectRock(_event: MouseEvent) {
     state[id] = 'rock'
     rockBtn.style.backgroundColor = '#d3df99'
     console.log(state)
+    checkGame()
   }
+
   function handleSelectPaper(_event: MouseEvent) {
     state[id] = 'paper'
     paperBtn.style.backgroundColor = '#d3df99'
     console.log(state)
+    checkGame()
   }
+
   function handleSelectScissors(_event: MouseEvent) {
     state[id] = 'scissors'
     scissorsBtn.style.backgroundColor = '#d3df99'
     console.log(state)
+    checkGame()
+  }
+  function handleStart(_event: MouseEvent) {
+    state.player1 = ''
+    state.player2 = ''
+    rockBtn.style.backgroundColor = '#474d64'
+    paperBtn.style.backgroundColor = '#474d64'
+    scissorsBtn.style.backgroundColor = '#474d64'
   }
 
   rockBtn?.addEventListener('click', handleSelectRock)
-
   paperBtn?.addEventListener('click', handleSelectPaper)
-
   scissorsBtn?.addEventListener('click', handleSelectScissors)
-  const tt = document.querySelector('button')
-  if (tt instanceof HTMLButtonElement) {
-    tt?.addEventListener('click', handleClickButton)
-  }
+  restartBtn?.addEventListener('click', handleStart)
 }
-
-// const restartGameButton = document.querySelector(
-//   '#restart'
-// ) as HTMLButtonElement
-
-// function restart(_event: Event) {
-//   state.player1 = ''
-//   state.player2 = ''
-// }
-// restartGameButton.addEventListener('click', restart)
-
-const winner = state.player1 || state.player2
-// const result = document.querySelector('.result') as HTMLElement
-
-function displayResult() {
-  if (state.player1 === '' || state.player2 === '') return
-  if (state.player1 === state.player2) {
-    ;('Tie')
-  } else if (state.player1 === 'rock' && state.player2 === 'paper') {
-    ;('Player 2 wins')
-  }
-  // else if (state.player1 === 'rock' && state.player2 === 'scissors') {
-  //   ;('Player 1 wins')
-  // } else if (state.player1 === 'scissors' && state.player2 === 'paper') {
-  //   ;('Player 1 wins')
-  // } else if (state.player1 === 'paper' && state.player2 === 'scissors') {
-  //   ;('Player 2 wins')
-  // }
-}
-
 setupRoshambo('player1')
 setupRoshambo('player2')
-// setupCounter('counter')
